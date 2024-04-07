4f507bf24a0e948fb0aef768bc46f6949a4b0f6b
lauer3912 <lauer3912@gmail.com>
2024-04-07 14:01:54 +0800

    init

--- submit.bing.js
+++ submit.bing.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 
-const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0'; // 替换为你的API密钥
-const siteHostName = 'android-transfer.techidaily.com'; // 替换为你的网站URL
+const indexNowKey = 'd286f9a66f69459f890341d578fb9eb6'; // 替换为你的API密钥
+const siteHostName = 'sim-unlock.techidaily.com'; // 替换为你的网站URL
 const groupItemCount = 100; // 每组最多个URL
 
 
